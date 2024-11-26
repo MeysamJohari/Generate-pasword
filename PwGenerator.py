8c092dbb1faa69bcdcc4ea102dcc70bbd06f517d
SPN <mr.johari67@gmail.com>
2024-11-26 17:41:30 +0330

    Second Way to genetate pasword

--- PwGenerator.py
+++ PwGenerator.py
@@ -1,12 +1,16 @@
 import random
 import string
 
+# #First way
+# def generate():  # define a function
+#     length = random.randint(16, 20)  # define a variable for length of password
+#     password = ''.join(random.choice(string.ascii_letters + string.digits)
+#                        for i in range(length))  # define a variable for password
+#     return password
 
-def generate():  # define a function
-    length = random.randint(16, 20)  # define a variable for length of password
-    password = ''.join(random.choice(string.ascii_letters + string.digits)
-                       for i in range(length))  # define a variable for password
-    return password
 
-
-print(generate())  # call the function
+# print(generate())  # call the function
+ # Second way
+listOfCaracters = string.ascii_letters + string.digits +string.punctuation
+pasword = ''.join(random.sample(listOfCaracters, 24))
+print(listOfCaracters)
